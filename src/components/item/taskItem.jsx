62931904c8194ffa38206cf08d1313695721aeb8
Damian2540 <chinitochitoo@gmail.com>
2023-11-22 15:11:55 -0300

    Vean que pueden hacer

--- src/components/item/taskItem.jsx
+++ src/components/item/taskItem.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { FaTrash } from 'react-icons/fa';
-import '../item/taskItem.css'
+import '../item/taskItem.css';
 
 const TaskItem = ({ task, onComplete, onDelete }) => {
-  
   if (!task) {
-    return null; 
+    return null;
   }
 
-  
   return (
-    < div className={`task ${task.completed ? 'completed' : ''}`}>
+    <div className={`task ${task.completed ? 'completed' : ''}`}>
       <span className='task-name'>{task.name}</span>
-      {!task.completed ? 
-      <button className='pending-button' onClick={() => onComplete(task.id)}>Pendiente</button> : <p className='completed-text'>Completo</p>}
+      <button className='status-button' onClick={() => onComplete(task.id)}>
+        {task.completed ? 'Completo' : 'Pendiente'}
+      </button>
       <button onClick={() => onDelete(task.id)} className='icon-delete'>
         <FaTrash />
       </button>
